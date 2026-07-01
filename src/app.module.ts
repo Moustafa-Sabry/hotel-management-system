6832705b27a35c8cc18885420ae8de84e0220292
Moustafa-Sabry <133038389+Moustafa-Sabry@users.noreply.github.com>
2026-07-01 18:02:17 +0300

    Initial hotel-management-system setup

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
+import { RoomModule } from './room/room.module';
+import { BookingModule } from './booking/booking.module';
+import { OfferModule } from './offer/offer.module';
+import { ReviewModule } from './review/review.module';
+import { FavouriteModule } from './favourite/favourite.module';
+import { FacilityModule } from './facility/facility.module';
+
+@Module({
+  imports: [DatabaseModule, UserModule, RoomModule, BookingModule, OfferModule, ReviewModule, FavouriteModule, FacilityModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/database/database.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+@Module({imports:[MongooseModule.forRoot(process.env.MONGODB_URI ?? 'mongodb://localhost:27017/hotel-management',),],})
+export class DatabaseModule {}
