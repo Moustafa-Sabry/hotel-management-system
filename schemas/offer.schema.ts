3c77975d4041c6357da16642027c2c1507120d4d
Moustafa-Sabry <133038389+Moustafa-Sabry@users.noreply.github.com>
2026-07-02 17:33:51 +0300

    Implement offer and facility schemas

---
+++ schemas/offer.schema.ts
@@ -0,0 +1,51 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+// import { Room } from './room.schema';
+
+export type OfferDocument = HydratedDocument<Offer>;
+
+@Schema({
+  timestamps: true,
+})
+export class Offer {
+  @Prop({
+    required: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 100,
+  })
+  title: string;
+
+  @Prop({
+    required: true,
+    trim: true,
+    maxlength: 1000,
+  })
+  description: string;
+
+  @Prop({
+    required: true,
+  })
+  image: string;
+
+  @Prop({
+    required: true,
+    min: 0,
+  })
+  price: number;
+
+  @Prop({
+    required: true,
+  })
+  startDate: Date;
+
+  @Prop({
+    required: true,
+  })
+  endDate: Date;
+
+//     @Prop({type: [{ type: Types.ObjectId, ref: Room.name }], default: [],})
+//     rooms: Types.ObjectId[];
+}
+
+export const OfferSchema = SchemaFactory.createForClass(Offer);
