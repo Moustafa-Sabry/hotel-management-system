5ca8ec0b97348a017236d0eb7509114c66ed4745
shereen <shereenelshahhat@gmail.com>
2026-07-09 19:27:58 +0300

    fixed app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,13 @@ import { OfferModule } from './offer/offer.module';
 import { ReviewModule } from './review/review.module';
 import { FavouriteModule } from './favourite/favourite.module';
 import { FacilityModule } from './facility/facility.module';
+import { RoomCategoriesModule } from './room-categories/room-categories.module';
 import { AuthModule } from './auth/auth.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, RoomModule, BookingModule, OfferModule, ReviewModule, FavouriteModule, FacilityModule, AuthModule,AuthModule, DashboardModule, MailModule ],
+  imports: [DatabaseModule, UserModule, RoomModule, BookingModule, OfferModule, ReviewModule, FavouriteModule, FacilityModule, AuthModule,RoomCategoriesModule, DashboardModule, MailModule ],
   controllers: [AppController],
   providers: [AppService],
 })
